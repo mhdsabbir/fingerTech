973a283334d7715579106e87f1ee932b044816e7
Mhd sabbir <muhammadsabbir666@gmail.com>
2022-03-14 16:11:57 +0600

    reprotExpo

--- src/UserCollection/Display.js
+++ src/UserCollection/Display.js
@@ -1,21 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
+import { CSVLink } from "react-csv";
+
+const headers = [
+    {label: 'stafId', key: 'stafId'},
+    {label: 'now', key: 'now'}
+]
+
 
 const Display = () => {
   const [sheet, setSheet] = useState([]);
+  const headings = ["Pin", "Time"];
 
+  const csvReport = {
+    filename: 'report.csv',
+    headers: headers,
+    data: sheet,
+
+}
 
 
   useEffect(() => {
-    const url = "http://localhost:5000/punchs";
+    const url = "https://boiling-cove-11605.herokuapp.com/punchs";
     fetch(url)
       .then((res) => res.json())
       .then((data) => setSheet(data));
   }, []);
-
-
+  
+  
+//   const exportOnclick = (e) =>{
+//       let result_table = sheet
+//       console.log(result_table);
+      
+    
+//   }
   return (
     <div>
+      {/* <button onClick={exportOnclick} type="submit">
+        Export Data
+      </button> */}
+      <CSVLink {...csvReport}>Export csv</CSVLink>
+
       <Table striped bordered hover>
         <thead>
           <tr>
