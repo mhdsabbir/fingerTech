e8add752824d8d06d54c7fe8f7b0497466c40af1
Mhd sabbir <muhammadsabbir666@gmail.com>
2022-03-17 23:24:42 +0600

    reprotEfaffadaoF

--- src/UserCollection/UserCollection.js
+++ src/UserCollection/UserCollection.js
@@ -4,8 +4,38 @@ import "./UserCollection.css";
 import Display from "./Display";
 
 const UserCollection = ({ date }) => {
+  let barcode = '';
+  let interval;
+  document.addEventListener('keydown', function(evt){
+    if (interval)
+      clearInterval(interval);
+    if(evt.code == 'Enter'){
+      if (barcode)
+      handleBarcode(barcode);
+      barcode = '';
+      return;
+    }
+    if(evt.code != 'Shift')
+      barcode += evt.key;
+      interval = setInterval(() => barcode = '', 20);
+    
+  });
+  function handleBarcode(scanned_barcode){
+    document.querySelector('#last-barcode').innerHTML = scanned_barcode;
+
+    
+  }
+
+  
   const form = useRef();
   const [success, setSuccess] = useState();
+  const [scanned , setScanned] = useState(
+    barcode
+  );
+
+  console.log(scanned.value);
+  console.log(scanned);
+  
 
   const [now, setNow] = useState(
     date.toLocaleDateString() +
@@ -23,26 +53,25 @@ const UserCollection = ({ date }) => {
     const stafId = pin;
     const newPunch = { stafId, now };
     
-
     
 
-    fetch("https://boiling-cove-11605.herokuapp.com/punchs", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(newPunch),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.insertedId) {
-          setSuccess(true);
-        }
-        if (data.insertedId) {
-          window.location.reload();
-          newPunch.clear();
-        }
-      });
+    // fetch("https://boiling-cove-11605.herokuapp.com/punchs", {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json",
+    //   },
+    //   body: JSON.stringify(newPunch),
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.insertedId) {
+    //       setSuccess(true);
+    //     }
+    //     if (data.insertedId) {
+    //       window.location.reload();
+    //       newPunch.clear();
+    //     }
+    //   });
 
     e.preventDefault();
   };
@@ -53,12 +82,32 @@ const UserCollection = ({ date }) => {
         <Row style={{ alignItems: "center", textAlign: "center" }}>
           <Col md={12}>
             <div className="l-form">
-              <form ref={form} onBlur={hangle} className="form">
+              <form ref={form}  className="form">
                 <div className="form__div">
-                  <input className="form__input" />
-                  <label htmlFor="number" className="form__label">
-                    Scan Your ID
+                <label for="name" class="form__label">
+                    Type
                   </label>
+                  <br />
+                  <input
+                    type="text"
+                    class="form__input"
+                    placeholder=" "
+                    name="name"
+                  />
+                  
+                  <br />
+                  <label for="barcode" class="form__label">
+                    Scan with barcode
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    class="form__input"
+                    placeholder=" "
+                    name="barcode"
+                    id="last-barcode"
+                  />
+                  
                 </div>
               </form>
             </div>
